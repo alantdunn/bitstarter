f8dc784ccd69c8b4c2e48f28f48b6f100dee1867
Alan Dunn <alan@limegreenenergy.com>
2013-07-05 23:19:11 +0000

    edited web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buffer = fsReadFile('index.html', function (err,data) {
+  buffer = fs.readFile('index.html', function (err,data) {
     if (err) throw err;
     response.send(buffertoString('utf-8'));
   });
@@ -14,3 +14,5 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
